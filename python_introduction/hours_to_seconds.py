f31e19062366153b714b4166eea3069b4785177a
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-05-25 20:33:19 +0300

    add hours_to_seconds.py script

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+#assigning values to variables
+hours = 2 # Number of hours
+seconds = 3600  # Number of seconds in one hour
+
+#calculating total seconds
+total_seconds = hours * seconds
+
+#printing the result
+print(f"Total seconds in {hours} hours is {total_seconds}")
