35c543a49ef7d1804eb930e564b540b2344093dc
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-05-27 19:44:51 +0300

    added conditional_statements_demo

---
+++ python_introduction/conditional_statements_demo.py
@@ -0,0 +1,10 @@
+#prompt user to input data 
+age = int(input("Please enter your age: "))
+
+#first conditional statement
+if age >= 18:
+    print("You are eligible to vote.")
+
+#second conditional statement
+else:
+    print("You are not eligible to vote.")
