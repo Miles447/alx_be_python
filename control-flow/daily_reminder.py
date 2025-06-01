01328db29fd9dcb5e74b2ac04a9b95ca98bd5555
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-06-01 20:24:44 +0300

    added daily reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,9 +1,9 @@
 #prompt user to enter a single task
 Task = input("Enter your Task: ")
 #prompt user to enter the task's priority
-Priority = input("Priority(high/medium/low): ")
+Priority = input("Priority (high/medium/low): ")
 #is the task time-bound
-Time_Bound = input("Is it Time Bound?(yes/no): ")
+Time_Bound = input("Is it Time Bound? (yes/no): ")
 #process task based on priority and time sensitivity
 match Priority:
     case "high":
