718c587ea0c0f8c5fd3913700b179ad8d95d2085
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-06-01 17:09:23 +0300

    added elif condition to weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,11 +2,11 @@
 weather = input("what's the weather like today? (sunny/rainy/cold): ")
 #match weather conditions to clothing recommendations.
 if weather == "sunny":
-    print("wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print("don't forget a raincoat and an umbrella")
+    print("Don't forget a raincoat and an umbrella.")
 elif weather == "cold":
-    print("make sure to wear a warm coat and a scarf")
+    print("Make sure to wear a warm coat and a scarf.")
 #if weather not in ["sunny", "rainy", "cold"]:
 else:
     print("I don't have recommendations for this weather.")
