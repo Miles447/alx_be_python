20f662f2892ba434a5a7c3aaeaad4bb18d231bdc
Miles Munyinyi <milesmunyinyi447@gmail.com>
2025-06-01 16:39:16 +0300

    added weather advice

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+#promp user to input current weather conditons.
+weather = input("what is the current weather condition? (sunny, rainy, cold): ")
+#match weather conditons to clothing recomendations.
+if weather == "sunny":
+    print("wear  t-shirt and sunglasses")
+if weather == "rainy":
+    print("dont forget a raincoat and an umbrella")
+if weather == "cold":
+    print("make sure to wear a warm coat and a scarf")
+#if weather not in ["sunny", "rainy", "cold"]:
+else:
+    print("I don't have recommendations for this weather.")
